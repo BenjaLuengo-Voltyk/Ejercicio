7848cc35f4b0a686eb7c657557d4239402dadd5a
CETECOM <cetecom_conce@duoc.cl>
2024-06-19 11:24:35 -0400

    funcion-suma

--- suma.py
+++ suma.py
@@ -1,7 +1,7 @@
 def suma():
     num1 = int(input("Ingrese el primer número: "))
     num2 = int(input("Ingrese el segundo número: "))
-    print("El resultado es:", num1, num2)
+    print("El resultado es:", num1 + num2)
 
 #TEST
 #suma()
